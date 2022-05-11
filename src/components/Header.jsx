a41e2cf28a1a074de1080692d3eed5a96551d970
YoimelDev <yoimeldev@gmail.com>
2022-05-11 19:43:07 -0400

    add Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+import styled from "@emotion/styled";
+
+const HeaderStyled = styled(Header)`
+    background-color: #fafafa;
+`
+
+export const Header = () => {
+    return (
+        <HeaderStyled>
+            <h1>Kimche Challenge</h1>
+        </HeaderStyled>
+    )
+}
