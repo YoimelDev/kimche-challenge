e9971724ad3b1a493f83a44882ce80720be32956
YoimelDev <yoimeldev@gmail.com>
2022-05-11 19:13:00 -0400

    add ApolloClient and ApolloProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import './index.css'
 
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/',
+  cache: new InMemoryCache()
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 )
