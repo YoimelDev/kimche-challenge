af9200f7d1f3a98e818b67319e9481f552abb658
YoimelDev <yoimeldev@gmail.com>
2022-05-11 21:12:58 -0400

    change properties of the svg and add handleSubmit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -27,16 +27,23 @@ const ButtonSubmit = styled.button`
 `
 
 export const Search = () => {
+
+    const handleSubmit = e => {
+        e.preventDefault()
+    }
+
     return (
 
-        <Form>
+        <Form
+            onSubmit={handleSubmit}
+        >
             <FormContent>
-                <InputSearch type="search" name="" id="" />
+                <InputSearch type="search" />
 
                 <ButtonSubmit
                     type="submit"
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2.5" stroke="#9e9e9e" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2.5" stroke="#9e9e9e" fill="none" strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <circle cx="10" cy="10" r="7" />
                         <line x1="21" y1="21" x2="15" y2="15" />
