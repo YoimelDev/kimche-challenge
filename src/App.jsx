1f782c83c7c1c7a1f4fa957951678ddcf3d94a1a
YoimelDev <yoimeldev@gmail.com>
2022-05-11 21:07:41 -0400

    add Search to App and add styles for Search

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Header } from "./components/Header"
+import { Search } from "./components/Search"
 
 function App() {
   return (
     <>
       <Header />
+
+      <Search />
     </>
   )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,48 @@
+import styled from "@emotion/styled"
+
+const Form = styled.form`
+    margin: 1.875rem 1.5625rem;
+    box-shadow: 0px 5px 10px -7px rgba(0,0,0,0.75);
+`
+
+const InputSearch = styled.input`
+    width: 100%;    
+    padding: 1.25rem .9375rem;
+    border: none;
+    border-radius: .3125rem;
+`
+
+const FormContent = styled.div`
+    position: relative;
+    height: 100%;
+`
+
+const ButtonSubmit = styled.button`
+    position: absolute;
+    left: 1.25rem;
+    height: 100%;
+    width: 3.125rem;
+    background-color: #fff;
+    border: none;
+`
+
 export const Search = () => {
     return (
-        <div>Search</div>
+
+        <Form>
+            <FormContent>
+                <InputSearch type="search" name="" id="" />
+
+                <ButtonSubmit
+                    type="submit"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2.5" stroke="#9e9e9e" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <circle cx="10" cy="10" r="7" />
+                        <line x1="21" y1="21" x2="15" y2="15" />
+                    </svg>
+                </ButtonSubmit>
+            </FormContent>
+        </Form>
     )
 }
