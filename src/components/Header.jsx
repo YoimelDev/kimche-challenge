7fb9c52db8737d537034fbcc540894aa6853e233
Yoimel Rojas <yoimeldev@gmail.com>
2022-05-12 15:37:42 -0400

    add responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,21 @@ import styled from "@emotion/styled"
 
 const HeaderStyled = styled.header`
     display: flex;
+    justify-content: center;
     align-items: center;
-    height: 10vh;
-    margin: 0;
-    padding: 0 1.5625rem;
+    height: 10vh;   
     background-color: #fff;
     box-shadow: 0px -10px 20px 0px rgb(0 0 0 / 75%);
 `
 
 const Title = styled.h1`  
+    width: min(85%, 75rem);
     font-size: 1.125rem;
     font-weight: 800;
+
+    @media (min-width: 768px) { 
+        font-size: 1.5rem;
+    }
 `
 
 export const Header = () => {
