727b58e2905dd73c73b8e13725a9aad60bed72aa
YoimelDev <yoimeldev@gmail.com>
2022-05-11 20:14:46 -0400

    add Header to App and add styles for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,24 @@
+import styled from "@emotion/styled"
 
-import styled from "@emotion/styled";
-
-const HeaderStyled = styled(Header)`
+const HeaderStyled = styled.header`
     background-color: #fafafa;
+`
+
+const Title = styled.h1`
+    display: flex;
+    align-items: center;
+    height: 15vh;
+    margin: 0;
+    padding: 0 1.5625rem;
+    background-color: #fff;
+    font-size: 1.5rem;
+    font-weight: 800;
 `
 
 export const Header = () => {
     return (
         <HeaderStyled>
-            <h1>Kimche Challenge</h1>
+            <Title>Country Search</Title>
         </HeaderStyled>
     )
 }
