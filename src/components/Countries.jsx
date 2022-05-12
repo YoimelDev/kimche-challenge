73699a1f9bf9d98eda944f9f5b6832f2d9bc3420
Yoimel Rojas <yoimeldev@gmail.com>
2022-05-12 15:32:40 -0400

    add styles and test info

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -2,12 +2,37 @@ import styled from "@emotion/styled"
 
 const Container = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
     gap: 1.25rem;
+    width: min(85%, 75rem);
+    margin: 0 auto;
 `
 
 const CountryCard = styled.div`
-    
+    width: 100%;
+    max-width: 20rem;
+    background-color: #fff;
+    border-radius: .3125rem;
+    overflow: hidden;
+`
+
+const CountryCardInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 2rem 1.5rem;
+
+    h2, p {
+        margin: 0;
+    }
+
+    p {
+        font-weight: 600;
+    }
+
+    span {
+        font-weight: 300;
+    }
 `
 
 export const Countries = () => {
@@ -15,8 +40,67 @@ export const Countries = () => {
         <Container>
             <CountryCard>
                 <div>
-                    <h2>Chile</h2>
+                    <img src="https://flagcdn.com/w320/cl.png" alt="Flag" />
                 </div>
+
+                <CountryCardInfo>
+                    <h2>Chile</h2>
+                    <p>Capital: <span>Santiago</span></p>
+                    <p>Currency: <span>CLF, CLP</span></p>
+                    <p>Phone: <span>+56</span></p>
+                </CountryCardInfo>
+            </CountryCard>
+
+            <CountryCard>
+                <div>
+                    <img src="https://flagcdn.com/w320/cl.png" alt="Flag" />
+                </div>
+
+                <CountryCardInfo>
+                    <h2>Chile</h2>
+                    <p>Capital: <span>Santiago</span></p>
+                    <p>Currency: <span>CLF, CLP</span></p>
+                    <p>Phone: <span>+56</span></p>
+                </CountryCardInfo>
+            </CountryCard>
+
+            <CountryCard>
+                <div>
+                    <img src="https://flagcdn.com/w320/cl.png" alt="Flag" />
+                </div>
+
+                <CountryCardInfo>
+                    <h2>Chile</h2>
+                    <p>Capital: <span>Santiago</span></p>
+                    <p>Currency: <span>CLF, CLP</span></p>
+                    <p>Phone: <span>+56</span></p>
+                </CountryCardInfo>
+            </CountryCard>
+
+            <CountryCard>
+                <div>
+                    <img src="https://flagcdn.com/w320/cl.png" alt="Flag" />
+                </div>
+
+                <CountryCardInfo>
+                    <h2>Chile</h2>
+                    <p>Capital: <span>Santiago</span></p>
+                    <p>Currency: <span>CLF, CLP</span></p>
+                    <p>Phone: <span>+56</span></p>
+                </CountryCardInfo>
+            </CountryCard>
+
+            <CountryCard>
+                <div>
+                    <img src="https://flagcdn.com/w320/cl.png" alt="Flag" />
+                </div>
+
+                <CountryCardInfo>
+                    <h2>Chile</h2>
+                    <p>Capital: <span>Santiago</span></p>
+                    <p>Currency: <span>CLF, CLP</span></p>
+                    <p>Phone: <span>+56</span></p>
+                </CountryCardInfo>
             </CountryCard>
         </Container>
     )
