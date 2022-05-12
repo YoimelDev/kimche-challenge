31c4b4041a82183700ee056e2b0dbebd2fa26b7d
YoimelDev <yoimeldev@gmail.com>
2022-05-11 22:23:05 -0400

    add styles to filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,23 @@
+import styled from "@emotion/styled"
+
+const Select = styled.select`
+    width: 12rem;
+    margin: 1.5625rem 1.5625rem;
+    padding: 1.25rem .625rem;
+    outline: none;
+    border: none;
+    border-radius: .3125rem;
+    box-shadow: 0px 5px 10px -7px rgba(0,0,0,0.75);
+`
+
+
 export const Filter = () => {
     return (
-        <div>Filter</div>
+        <form>
+            <Select>
+                <option value="continents">Continent</option>
+                <option value="language">Language</option>
+            </Select>
+        </form>
     )
 }
