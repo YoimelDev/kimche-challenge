cb3ca30f4220f40c322f75b02ac9f406edb36720
YoimelDev <yoimeldev@gmail.com>
2022-05-11 20:23:03 -0400

    add box shadow to header and background to body

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import styled from "@emotion/styled"
 
 const HeaderStyled = styled.header`
-    background-color: #fafafa;
-`
-
-const Title = styled.h1`
     display: flex;
     align-items: center;
-    height: 15vh;
+    height: 10vh;
     margin: 0;
     padding: 0 1.5625rem;
     background-color: #fff;
+    box-shadow: 0px -10px 20px 0px rgb(0 0 0 / 75%);
+`
+
+const Title = styled.h1`  
     font-size: 1.125rem;
     font-weight: 800;
 `
