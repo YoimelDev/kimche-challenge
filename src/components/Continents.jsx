f62d192208d5fda8573146e236d052443fa48918
YoimelDev <yoimeldev@gmail.com>
2022-05-12 20:54:29 -0400

    add useCountries to Continents and delete old funtion of Countries

--- src/components/Continents.jsx
+++ src/components/Continents.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import styled from "@emotion/styled"
+import { useCountries } from "../graphql/custom-hooks"
+import { Countries } from "./Countries"
 
-export const Continents = () => {
+const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+`
+
+export const Continents = ({ continents }) => {
+
+    const { data, error, loading } = useCountries()
+
     return (
-        <div>Continents</div>
+        <Section>
+            {continents.map(({ name, code }) => {
+                return (
+                    <Section key={code}>
+                        <h2>{name}</h2>
+
+                        <Countries
+                            countries={data.countries}
+                            continentName={name}
+                        />
+                    </Section>
+                )
+            })}
+        </Section>
     )
 }
--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,83 +1,9 @@
 import styled from "@emotion/styled"
-import { useContinents } from "../graphql/custom-hooks"
 
-const Container = styled.div`
-    width: min(85%, 75rem);
-    margin: 0 auto;
-`
-
-const CountryCard = styled.div`
-    width: 100%;
-    max-width: 20rem;
-    background-color: #fff;
-    border-radius: .3125rem;
-    overflow: hidden;
-`
-
-const CountryCardInfo = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    padding: 2rem 1.5rem;
-
-    h2, p {
-        margin: 0;
-    }
-
-    p {
-        font-weight: 600;
-    }
-
-    span {
-        font-weight: 300;
-    }
-`
-
-const Section = styled.section`
-    display: flex;
-    flex-direction: column;
-`
-
-const CountriesContainer = styled.div`
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
-    gap: 1.25rem;
-`
-
-export const Countries = () => {
-
-    const { data, error, loading } = useContinents()
-
+export const Countries = ({ countries, continentName }) => {
     return (
-        <Container>
-
-            {/* {
-                data.continents.map(({ name, code }) => {
-                    return (
-                        <Section key={code}>
-                            <h2>{name}</h2>
-                        </Section>
-                    )
-                })
-            } */}
-            {/* <Section>
-                <h2>South America</h2>
-
-                <CountriesContainer>
-                    <CountryCard>
-                        <div>
-                            <img src="https://flagcdn.com/w320/cl.png" alt="Flag" />
-                        </div>
-
-                        <CountryCardInfo>
-                            <h2>Chile</h2>
-                            <p>Capital: <span>Santiago</span></p>
-                            <p>Currency: <span>CLF, CLP</span></p>
-                            <p>Phone: <span>+56</span></p>
-                        </CountryCardInfo>
-                    </CountryCard>
-                </CountriesContainer>
-            </Section> */}
-        </Container>
+        <>
+            <h1>Countries</h1>
+        </>
     )
 }
