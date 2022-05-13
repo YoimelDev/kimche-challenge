1e578de6a46e24601c849a543191a43a3bc7d1a4
YoimelDev <yoimeldev@gmail.com>
2022-05-12 22:45:16 -0400

    add Languages component

---
+++ src/components/Languages.jsx
@@ -0,0 +1,35 @@
+import styled from "@emotion/styled"
+import { useCountries } from "../graphql/custom-hooks"
+import { Countries } from "./Countries"
+import { Spinner } from "./Spinner"
+
+const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+`
+
+export const Languages = ({ languages }) => {
+
+    const { data, error, loading } = useCountries()
+
+    return (
+        <Section>
+            {languages.map(({ name, code }) => {
+                return (
+                    <Section key={code}>
+                        <h2>{name}</h2>
+
+                        {
+                            loading
+                                ? <Spinner />
+                                : <Countries
+                                    countries={data.countries}
+                                    lenguageName={name}
+                                />
+                        }
+                    </Section>
+                )
+            })}
+        </Section>
+    )
+}
