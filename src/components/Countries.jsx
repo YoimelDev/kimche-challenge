2fe151c88ad880c4716f8299f9d4aaec5c122d82
YoimelDev <yoimeldev@gmail.com>
2022-05-14 17:55:27 -0400

    refactor: add component Country

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,18 +1,5 @@
 import styled from '@emotion/styled'
-import { motion } from 'framer-motion'
-
-const variants = {
-	hidden: {
-		opacity: 0
-	},
-	show: ({ delay }) => ({
-		opacity: 1,
-		transition: {
-			delay,
-			duration: 1
-		}
-	})
-}
+import { Country } from './Country'
 
 const CountriesContainer = styled.div`
 	display: grid;
@@ -21,71 +8,20 @@ const CountriesContainer = styled.div`
 	gap: 1.25rem;
 `
 
-const CountryCard = styled(motion.div)`
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	width: 100%;
-	max-width: 20rem;
-	background-color: #fff;
-	border-radius: 0.3125rem;
-	overflow: hidden;
-`
-
-const CountryCardInfo = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 1rem;
-	padding: 2rem 1.5rem;
-
-	h2,
-	p {
-		margin: 0;
-	}
-
-	p {
-		font-weight: 600;
-	}
-
-	span {
-		font-weight: 300;
-	}
-`
-
 export const Countries = ({ countries }) => {
 	return (
 		<CountriesContainer>
-			{countries.map(({ name, code, phone, capital, currency }, index) => {
-				return (
-					<CountryCard
-						key={code}
-						custom={{ delay: (index + 1) * 0.2 }}
-						initial="hidden"
-						animate="show"
-						variants={variants}
-					>
-						<div>
-							<img
-								src={`https://flagcdn.com/w320/${code.toLowerCase()}.webp`}
-								alt='Flag'
-							/>
-						</div>
-
-						<CountryCardInfo>
-							<h2>{name}</h2>
-							<p>
-								Capital: <span>{capital}</span>
-							</p>
-							<p>
-								Currency: <span>{currency}</span>
-							</p>
-							<p>
-								Phone: <span>+{phone}</span>
-							</p>
-						</CountryCardInfo>
-					</CountryCard>
-				)
-			})}
+			{countries.map(({ name, code, phone, capital, currency }, index) => (
+				<Country
+					key={code}
+					name={name}
+					code={code}
+					phone={phone}
+					capital={capital}
+					currency={currency}
+					index={index}
+				/>
+			))}
 		</CountriesContainer>
 	)
 }
