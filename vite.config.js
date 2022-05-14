1e8e9ef5c23b749ba0430b68e640862c936ef3ba
YoimelDev <yoimeldev@gmail.com>
2022-05-14 19:12:30 -0400

    build: add base and outDir

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: 'https://YoimelDev.github.io/kimche-challenge/',
+	build: {
+		outDir: 'docs'
+	}
 })
