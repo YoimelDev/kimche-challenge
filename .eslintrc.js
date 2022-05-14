b464f1ec53f2a928e68efcfc9d0e19bad4574cc4
YoimelDev <yoimeldev@gmail.com>
2022-05-14 11:40:25 -0400

    build: add prettier config and change modified eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'standard'
   ],
   parserOptions: {
@@ -18,6 +19,5 @@ module.exports = {
     'react'
   ],
   rules: {
-    "indent": ["error", 2]
   }
 }
