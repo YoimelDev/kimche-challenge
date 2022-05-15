cd5ff26655f8f6799ccdf3a2548ebf7dbe9fadd9
YoimelDev <yoimeldev@gmail.com>
2022-05-14 20:18:29 -0400

    perf: add limit for show countries

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -11,17 +11,21 @@ const CountriesContainer = styled.div`
 export const Countries = ({ countries }) => {
 	return (
 		<CountriesContainer>
-			{countries.map(({ name, code, phone, capital, currency }, index) => (
-				<Country
-					key={code}
-					name={name}
-					code={code}
-					phone={phone}
-					capital={capital}
-					currency={currency}
-					index={index}
-				/>
-			))}
+			{countries.map(({ name, code, phone, capital, currency }, index) => {
+				return (
+					index <= 7 && (
+						<Country
+							key={code}
+							name={name}
+							code={code}
+							phone={phone}
+							capital={capital}
+							currency={currency}
+							index={index}
+						/>
+					)
+				)
+			})}
 		</CountriesContainer>
 	)
 }
